125173c3300eadf115e197c2630f562e2c1b76ba
Chandramohan Jha <chandramohanjha036@gmail.com>
2022-12-23 21:18:28 +0530

    first commit

---
+++ src/components/Layout/FooterMob.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+// Images
+import Path_115225 from '../../Images/Path_115225.svg';
+import facebook from "../../Images/Icon awesome-facebook-square.svg";
+import twitter from "../../Images/Icon awesome-twitter.svg";
+import linkdin from "../../Images/Icon awesome-linkedin.svg";
+import instagram from "../../Images/Icon awesome-instagram.svg";
+
+const FooterMob = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="footer_mob">
+        <div className="container">
+          <div
+            className="d-flex justify-content-center sm-pb2"
+            onClick={() => navigate("/")}
+          >
+            <img src={Path_115225} alt="logo" width="164px" height="44px" />
+          </div>
+          <div
+            className="d-flex align-items-center justify-content-between footer_mob_border sm-pb2"
+            style={{ width: "100%", paddingTop: "20px", cursor: "pointer" }}
+          >
+            <img src={facebook} alt="icons" width="30px" height="30px" />
+            <img src={twitter} alt="icons" width="34px" height="27px" />
+            <img src={linkdin} alt="icons" width="27px" height="27px" />
+            <img src={instagram} alt="icons" width="30px" height="30px" />
+          </div>
+          <div className="footer_mob_border sm-pt2">
+            <div className="sm-14 text-center">
+              <b>OM SAI CORPORATION© 2021. All rights Reserved.</b>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FooterMob;
